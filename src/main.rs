899e3bb14b85b6f8182d81f17596e2d2a8d9dc62
PraveenRoyal <praveenpp2929@gmail.com>
2024-05-02 15:18:50 +0200

    first commit

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,14 @@ use aisysproj::create_state;
 pub mod state;
 
 fn main() {
-    let path = Path::new("problem_e_19.txt");
+/*    let path = Path::new("problem_e_19.txt");
     let mut x = create_state(path);
     let y = x.find_plan();
-    println!("{}",y.unwrap())
+    println!("{}",y.unwrap());*/
+
+    let f = std::path::Path::read_dir(Path::new("."));
+    for x in f.unwrap(){
+        println!("{:?}",x.unwrap().path());
+    }
+
 }
