a2240ac7b0952eb8b35af3cd3e1112015e1c4d2d
PraveenRoyal <praveenpp2929@gmail.com>
2024-05-02 14:50:04 +0200

    first commit

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use aisysproj::create_state;
 pub mod state;
 
 fn main() {
-    let path = Path::new("problem_e_07.txt");
+    let path = Path::new("problem_e_19.txt");
     let mut x = create_state(path);
     let y = x.find_plan();
     println!("{}",y.unwrap())
